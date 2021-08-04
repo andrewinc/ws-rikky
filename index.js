2cc4928a5211a3a28f59261b930d4905e73a3f86
Ocheretnyi <a.ocheretnyi@pmvent.ru>
2021-08-04 12:10:28 +0300

    Send message by token

--- index.js
+++ index.js
@@ -3,61 +3,8 @@ const http = require('http')
 const https = require('https')
 const fs = require('fs')
 const WebSocket = require('ws')
-const mysql = require('mysql')
-const con = mysql.createConnection(config.DB)
 console.log(config)
 const token = Symbol('token')
-const user_id = Symbol('id')
-//установка соединения с БД
-con.connect(err=>{
-    if (err) throw err
-    console.log("Database connected")
-
-    /** Поиск токена в таблице БД и возврат его ID через Promise
-     * ```
-     * getId('token2').then(id=>console.log('ID:',id), e=>console.error('Error', e)) // печать 'ID: 2'
-     * ```
-     * @param {string} token Ex.: 'token1'
-     * @return {Promise} */
-    const getId= async token => new Promise((resolve, reject) => {
-        con.query("SELECT id FROM users WHERE token = ?", [token],(err, rows)=>{
-            if (err) reject(err)
-            if (1 != rows.length) reject(new Error('Not found token or have dublicate'))
-            try {
-                const row = rows[0]
-                if (('id' in row) && row.id) resolve(row.id); else reject(new Error('Not found field or value is empty'))
-            }catch (e) {
-                reject(e)
-            }
-        })
-    })
-
-    /** Разобрать search строку в объект
-     * ```
-     * getUrlParams('') //{}
-     * getUrlParams('/') //{}
-     * getUrlParams('/?') //{}
-     * getUrlParams('/?a') //{}
-     * getUrlParams('/?a=') //{a:''}
-     * getUrlParams('?a=') //{a:''}
-     * getUrlParams('/?a=Hi') //{a:'Hi'}
-     * getUrlParams('?a=Hi') //{a:'Hi'}
-     * getUrlParams('/?a=Hi&b=12') //{a:"Hi", b:"12"}
-     * getUrlParams("?title=VUE&action=edit&section=18") //{title:"VUE", action:"edit", section:"18"}
-     * ```
-     * @param {string} search строка типа location.search Ex.: '?title=VUE&action=edit&section=18'
-     * @return {Object} Ex.: {title:"VUE", action:"edit", section:"18"} */
-    const getUrlParams = search => {
-        const params = {}
-        const hashes = search.slice(search.indexOf('?') + 1).split('&')
-        hashes.filter(hash=>hash.includes('=')).map(hash => {
-            const [key, val] = hash.split('=')
-            if ('' != key) {
-                params[key] = decodeURIComponent(val)
-            }
-        })
-        return params
-    }
 
     //создание WebSocket-сервера поверх SSL или нет
     const wss = new WebSocket.Server(
@@ -77,19 +24,31 @@ con.connect(err=>{
     wss.on('connection', (wsBrowser, req) => {
         wsBrowser.sendObj= obj =>{ try{wsBrowser.send(JSON.stringify(obj))}catch(e){ try{wsBrowser.close()}catch(e){}} }
 
-        //token
-        let urlParams = getUrlParams(req.url) //'/' => {} '/?token=token1'=>{token:'token1'}
-        wsBrowser[token] = urlParams.token
-        getId(wsBrowser[token]).then(id=>wsBrowser[user_id] = id,e=>wsBrowser[user_id]=undefined)// вычислить по токену ID
-
         //приветсвие при коннекте
-        if (wsBrowser[token]) wsBrowser.send(`Hi browser "${wsBrowser[token]}"!`); else wsBrowser.send(`Hi browser! Your not have token => not have messages`)
+        wsBrowser.send(`Hi browser!`)
+        wsBrowser.on('message', message=>{
+            try{
+              let msg = JSON.parse(message) // received JSON
+              if (('setToken' == msg.cmd) && msg.data) {
+                  wsBrowser[token] = msg.data
+                  wsBrowser.send(`Accept token `+JSON.stringify(msg.data))
+              }
+            } catch (e) { //received TEXT
+
+            }
+        })
+        wsBrowser.on('close', ()=>{console.log('client disconnected')})
+
+    })
+
+    wss.on('error', function(){
+        console.log('Error:', arguments)
     })
 
 
 
 
-
+    //Backend-server на которй отправляет запрос api
     const server = http.createServer((req, res) => {
         let body = ''
         req.on('data', data=>{
@@ -105,10 +64,10 @@ con.connect(err=>{
             //body = `{"id":2,"type":"t1","text":"Hi","link":"https://ya.ru"}`
             try{
                 let json = JSON.parse(body) //получили POST запрос с ID клиента
+                // console.log('POST HTTP request', json)
                 wss.clients.forEach(wsBrowser =>{
-
-                    if (json.id == wsBrowser[user_id]){
-                        wsBrowser.send(body) // отправляем сообщение только тому клиенту что найден по token\ID
+                    if (wsBrowser[token] && (wsBrowser[token] == json.to)) {
+                        wsBrowser.send(body) // отправляем сообщение только тому клиенту (или клентам) что найден по token
                     }
                 })
             res.statusCode = 200
@@ -129,4 +88,3 @@ con.connect(err=>{
     })
 
 
-})
--- index.php
+++ index.php
@@ -6,47 +6,23 @@ if ($code = mysqli_connect_errno()) {
     echo mysqli_connect_error();
     exit();
 }
-$token_current = isset($_GET['token']) ? $_GET['token'] : '';
-
 $ws_url = "ws" . ($config->WS->ssl_use?'s':'') . "://{$config->WS->host}:{$config->WS->port}";
-if ($token_current) $ws_url .= "/?token=" . $token_current;
 
-$replace = [
-    '%%WS_URL%%' => "const WS_URL = ".json_encode($ws_url),
-    '%%tokens%%' => '',
-    '%%options%%'=> '',
-];
-$li = [];
-$option =[];
-$st = $mysqli->prepare("SELECT id, token FROM users");
+$tokens=[];
+$st = $mysqli->prepare("SELECT token FROM users");
 try{
     if ($st->execute()) {
-        $st->bind_result($id, $token);
+        $st->bind_result($token);
         while ($st->fetch()) {
-            $li[] = sprintf(
-                '<li class="%s"><a href="/?token=%s">ID: %d; TOKEN: %s</a></li>',
-                ($token_current == $token) ? 'focus' : '',
-                $token,
-                $id,
-                $token
-            );
-            $option[] = sprintf('<option>%d</option>', $id);
+            $tokens[] = $token;
         }
     }
 } finally {
     $st->close();
 }
 
-
-
-$replace['%%tokens%%'] .= join("\n", $li);
-$replace['%%options%%'] .= join("\n", $option);
 echo str_replace(
-    array_keys($replace),
-    array_values($replace),
+    '%%SCRIPT%%',
+    "const WS_URL = ".json_encode($ws_url)."\nconst TOKENS=".json_encode($tokens),
     file_get_contents('template.html')
 );
-//print_r($config);
-
-
-//phpinfo();
